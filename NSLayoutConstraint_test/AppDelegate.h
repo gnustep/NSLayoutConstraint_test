ced557e600ea3a6d5bf5b8d608ad6cd37323f17c
Gregory Casamento <greg.casamento@gmail.com>
2020-05-10 09:33:01 -0400

    Initial Commit

---
+++ NSLayoutConstraint_test/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  NSLayoutConstraint_test
+//
+//  Created by Gregory Casamento on 5/10/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+
+@end
+
---
+++ NSLayoutConstraint_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSLayoutConstraint_test
+//
+//  Created by Gregory Casamento on 5/10/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
