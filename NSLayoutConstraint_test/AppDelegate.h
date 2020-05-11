383100346469be112782076cce2fea08aa17c377
Gregory John Casamento <greg.casamento@gmail.com>
2020-05-11 04:14:39 -0400

    Remove core data stuff

--- NSLayoutConstraint_test/AppDelegate.h
+++ NSLayoutConstraint_test/AppDelegate.h
@@ -7,12 +7,7 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#import <CoreData/CoreData.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-
-@property (readonly, strong) NSPersistentContainer *persistentContainer;
-
-
 @end
 
